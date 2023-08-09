6edc4f7eb506a5209ff60f717dcd2c65e26d9a03
nduka145 <nduka1455@gmail.com>
2023-08-09 15:50:31 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "testingclass45"
+    key    = "myvpctest/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "letstest" {
+  cidr_block = "10.0.0.0/16"
+  
+    tags = {
+    Name = "githubactions"
+  }
+}

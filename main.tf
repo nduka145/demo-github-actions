b513d6f1fd10abda1d7c6f39fbe06295d5e1ed32
nduka145 <nduka1455@gmail.com>
2023-08-09 16:02:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "letstest" {
   cidr_block = "10.0.0.0/16"
-  
-    tags = {
-    Name = "githubactions"
+    tags     = {
+    Name     = "githubactions"
   }
 }

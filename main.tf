a2b1b27041aa0ab3757fc035dde365920e82fde4
nduka145 <nduka1455@gmail.com>
2023-08-13 19:00:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,27 @@ terraform {
     region = "us-east-1"
   }
 }
-# Create a VPC
-resource "aws_vpc" "letstest" {
-  cidr_block = "10.0.0.0/16"
-    tags     = {
-    Name     = "githubactions"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0905273034904bd70"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }

214437361ecfb3e0731c94731adf468d2151087c
nduka145 <nduka1455@gmail.com>
2023-08-12 22:32:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "testingclass45"
+    key    = "1236/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 # Create a VPC
 resource "aws_vpc" "letstest" {
   cidr_block = "10.0.0.0/16"
